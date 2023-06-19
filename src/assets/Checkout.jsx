049453b482471f76aea568c214acafb76ce2cb25
adityadesign <68054763+adityadesign@users.noreply.github.com>
2023-06-20 01:43:05 +0530

    CheckoutImage

--- src/assets/Checkout.jsx
+++ src/assets/Checkout.jsx
@@ -4,7 +4,7 @@ export default function Checkout(){
     return (
         <div className="checkout">
             <h2>Order Summary</h2>
-            <img className="checkoutImg" src='/src/assets/checkout.png' alt="" />
+            <img className="checkoutImg" src='../public/checkout.png' alt="" />
             <h3>Thank you, your order has been placed.</h3>
             <p>Your order has been successfully completed. Within moments you will receive a notification with the receipt of your purchase and track every step of your order.</p>
             <Link to='/'><button>Go to Home</button></Link>
